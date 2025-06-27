aade9875dc72c68f946a9a0e4136ce09ba39e767
Ralph Oliveira <ralph.venancio@gmail.com>
2025-06-27 20:27:35 +0100

    Add data factory + pipeline

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,17 @@
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" this {
   name     = var.az_rg_name
-  location = "West Europe"
+  location = var.az_location
 }
 
+resource "azurerm_data_factory" this {
+  name = var.az_data_factory_name
+  location = var.az_location
+  resource_group_name = var.az_rg_name
+}
+
+resource "azurerm_data_factory_pipeline" this {
+  name = var.az_data_fact_pipeline_name
+  data_factory_id = azurerm_data_factory.this.id
+}
